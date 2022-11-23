76faab260ca5c2f413decb65495c7c5f39498f4f
isabellafsc <isabellafscarpari@gmail.com>
2022-11-23 12:31:18 -0300

    aula04

--- Arquivos/app01/src/App.js
+++ Arquivos/app01/src/App.js
@@ -1,12 +1,35 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
+import Imagem1 from './components/imgs/imagem1.png'
 
 export default function App() {
+  function hello() {
+    return 'Hello World'
+  }
+
+  const canal = () => {
+    return 'CFB Cursos'
+  }
+
+  const curso = 'Curso de React'
+
   return (
     <>
-      <p>Hello World</p>
-      <br/>
-      <p> Curso de React</p>
+      <section>
+        <header>
+          <p> {hello()}</p>
+          <br />
+          <p>Canal: {canal()}</p>
+          <p> {'Curso: ' + curso}</p>
+        </header>
+
+        <section>
+          {/* pasta src */}
+          <img src={Imagem1} />
+          {/* pasta public */}
+          <img src="/imagem2.png" />
+        </section>
+      </section>
     </>
   )
 }
-
