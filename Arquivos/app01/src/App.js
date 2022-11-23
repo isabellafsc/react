6eb5ef79a896a0a621f3c920ab4753fecdc43ef6
isabellafsc <isabellafscarpari@gmail.com>
2022-11-23 12:39:29 -0300

    Aula05

--- Arquivos/app01/src/App.js
+++ Arquivos/app01/src/App.js
@@ -1,35 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import Imagem1 from './components/imgs/imagem1.png'
+import Body from './components/Body'
+import Header from './components/Header'
 
 export default function App() {
-  function hello() {
-    return 'Hello World'
-  }
-
-  const canal = () => {
-    return 'CFB Cursos'
-  }
-
-  const curso = 'Curso de React'
-
   return (
     <>
-      <section>
-        <header>
-          <p> {hello()}</p>
-          <br />
-          <p>Canal: {canal()}</p>
-          <p> {'Curso: ' + curso}</p>
-        </header>
-
-        <section>
-          {/* pasta src */}
-          <img src={Imagem1} />
-          {/* pasta public */}
-          <img src="/imagem2.png" />
-        </section>
-      </section>
+      <Header />
+      <Body />
     </>
   )
 }
---
+++ Arquivos/app01/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Imagem1 from './imgs/imagem1.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={Imagem1}></img>
+      <p>CFB Cursos</p>
+    </header>
+  )
+}
