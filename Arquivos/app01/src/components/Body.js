973caebf4d2ab55b633160d53426e29069f31c2d
isabellafsc <isabellafscarpari@gmail.com>
2022-11-29 13:03:59 -0300

    aula07

--- Arquivos/app01/src/components/Body.js
+++ Arquivos/app01/src/components/Body.js
@@ -4,12 +4,20 @@ import Dados from './Dados'
 export default function Body() {
   const ytb = 'youtube.com/cfbcursos'
 
+  const cnl = () => {
+    return 'CFB Cursos'
+  }
+
+  const somar = (v1, v2) => {
+    return v1 + v2
+  }
+
   return (
     <section>
       <h2> Curso de React</h2>
       <p> Hello World </p>
 
-      <Dados canal="CFB Cursos" youtube={ytb} curso="React.js" />
+      <Dados canal={cnl} youtube={ytb} curso="React.js" somar={somar} />
     </section>
   )
 }
