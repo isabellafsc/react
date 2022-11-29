4e9ec1d5624f4797bc7dbc0cac704211180b7a03
isabellafsc <isabellafscarpari@gmail.com>
2022-11-29 13:31:20 -0300

    aula08

--- Arquivos/app01/src/App.js
+++ Arquivos/app01/src/App.js
@@ -1,16 +1,23 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import Body from './components/Body'
-import Header from './components/Header'
-import Teste from './components/Teste'
+import './App.css'
 
 export default function App() {
+  const textoDestaque = {
+    color: 'blue',
+    fontSize: '3em'
+  }
 
   return (
     <>
-      <Header />
-      <Body />
-      <Teste nome='Oi'/>
+      <section className='caixa'>
+        <h1 style={{ color: 'red', fontSize: '5em' }}> CFB Cursos</h1>
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className="texto">
+          {' '}
+          Se inscreva em nosso canal e nos siga no instagram
+        </p>
+        <a href='#' target='_blank'> Hello World</a>
+      </section>
     </>
   )
 }
